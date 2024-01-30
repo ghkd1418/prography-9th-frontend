29465c8b67f3f7d2ff4d362298018b6e9c484415
ghkd1418 <1004wipi@gmail.com>
2024-01-30 17:05:03 +0900

    feat: 헤더 구현[#1] 로고 클릭 시 '/'로 이동

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,16 @@
+import { NavLink, Outlet } from 'react-router-dom'
+
+function Header() {
+  return (
+    <>
+      <nav>
+        <NavLink to='/'>
+          <img src='images/prography.webp' alt='프로그라피 로고' />
+        </NavLink>
+      </nav>
+      <Outlet />
+    </>
+  )
+}
+
+export default Header
